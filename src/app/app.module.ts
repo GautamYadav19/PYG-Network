c9b666b715b3acef114b35b6de6d8d7aae607405
Anirudh919 <anirudhkashyap67@gmail.com>
2025-07-26 01:52:30 +0530

    add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './core/components/landing-page/landing-page.component';
+import { ProductListComponent } from './core/components/product/product-list/product-list.component';
+import { ProductDetailsComponent } from './core/components/product/product-details/product-details.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { LoginSignupComponent } from './core/components/auth/login-signup/login-signup.component';
+import { ResetComponent } from './core/components/auth/reset/reset.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginSignupComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
